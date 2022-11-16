1665cb9966219ba937e6887cceef408586e0e36b
meka <tamieka.horne@hotmail.com>
2022-11-16 05:31:36 +0000

    About complex lists

---
+++ challenges/tuesdaychallenge.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+mylist= [1,2,3,4,5, "Python"]
+
+name= input("What is your name?\n>")
+
+# This is what you should see when print runs-
+
+# Hi Meka ! Welcome to Day 2 of Python Training!
+print(f"Hi " + meka.capitalize + "! Welcome to Day " + mylist[1] + " of " + mylist[5] + " Training!")
+
