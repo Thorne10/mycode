0bd303c9a4fbf6b8a1e5ccfd9ac5c6d475c715df
meka <tamieka.horne@hotmail.com>
2022-11-16 18:48:59 +0000

    learning about dictionaries

---
+++ challenge42.py
@@ -0,0 +1,14 @@
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+
+a= challenge[2] [1]
+b= challenge[2] [0]
+c =challenge[3]
+
+print
