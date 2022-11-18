5a06853456d660410b028260b8a7c1466b1a1df5
meka <tamieka.horne@hotmail.com>
2022-11-18 15:41:30 +0000

    backup

---
+++ materialgirl/materialgirl_python3.py
@@ -0,0 +1,50 @@
+# first step is to generate questions
+#!/usr/bin/env python3
+import html
+
+# it's because there's a whitespace in your variable. either make it "material_girl" or delete lines 6-8
+material_girl= {
+        "vehicle": " Luxury: gas_guzzlere",
+        "Brunch": " Luxury: Yaht",
+
+        }
+
+vehicles= { "Luxury Gas Guzzler": True,
+            "Tesla": True,
+            "Hybrid": False,
+            "American Sedan": False,}
+
+Brunch= { "Yaht": True,
+          "Steakhouse": True,
+          "Ihop": False,
+          "Broken Yoke": False,}
+
+("vehicles",["Tesla"]["Luxury Gas Guzzler"])
+if  True:
+        print ("Material Girl or Guy!")
+
+        ("vehicles",{"Hybrid"}{"American Sedan"})                
+if False:
+       print ("Smart Guys/Gals Budget!")
+
+
+("Brunch",{"Yaht"}{"Steakhouse"})
+
+if True:
+    print ("Material Girl or Guy!")
+
+("Bruch",{"Ihop"}{"Broken Yoken"})
+    
+    
+
+    #print the keys from that dictionary
+# print a question, which would you choose?
+
+# use an if to see if it returns true or false
+
+# examples:
+   # if vehicles[choice] == True:  material_girl_score +1... or print out "material girl!!"
+   # elif vehicles[choice] == False: material_girl_score -1... or print out "not material girl!!"
+
+   
+                        
